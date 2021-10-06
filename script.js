57d0eab47d8da50e929a4f5a17aafbe49bcb9216
Abigail Paulus <91631177+abbypaulus@users.noreply.github.com>
2021-10-06 15:03:51 -0400

    removed

--- script.js
+++ script.js
@@ -35,26 +35,4 @@ function validateForm() {
   }
 }
 
-var myText = document.getElementById("my-text");
-var result = document.getElementById("result");
-
-var limit = 100
-
-result.textContent = 0 +"/" + limit
-
-myText.addEventListener("input",function(){
-
-  var textLength = myText.value.length;
-
-  result.textContent = textLength + "/" + limit;
-
-  if(textLength > limit){
-      myText.style.borderColor = "#ff2851";
-      result.style.color = "#ff2851";
-  }
-  else{
-      myText.style.borderColor = "#31821b";
-      result.style.color = "#31821b";
-  }
-});
 
