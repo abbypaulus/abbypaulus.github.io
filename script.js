535436beee131d9a4308c696c89fd29e32c7b9ef
Abigail Paulus <91631177+abbypaulus@users.noreply.github.com>
2021-11-10 15:55:39 -0500

    accordion

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-var acc = document.getElementsByClassName('accordionC');
+var acc = document.getElementsByClassName("accordion");
 var i;
 
-for (i = 0; i < acc.length; i++){
-  acc[i].addEventListener('click', function(){
-    this.classList.toggle('active');
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.display === 'block') {
-      panel.style.display = 'none';
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
     } else {
-      panel.style.display = 'block';
+      panel.style.display = "block";
     }
   });
-
-
 }
 
 
