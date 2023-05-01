846ed2134c46e368939d2ace2de4475a99fad809
Abigail Paulus <abbypaulus@icloud.com>
2023-05-01 01:21:30 -0400

    Heading and Footer

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+const canvas = document.querySelector('canvas')
 
+const c = canvas.getContext('2d')
+
+addEventListener('resize', () => {
+    canvas.width = innerWidth
+    canvas.height = innerHeight
+  })
+  
+c.rect(200,200,200,200);
+c.fill(255,00,0);
 
