91ff369e5e06678eb27d4ee5840a3a5b6e848832
Abigail Paulus <91631177+abbypaulus@users.noreply.github.com>
2023-09-20 09:41:12 -0400

    moved var to before calling

--- script.js
+++ script.js
@@ -14,12 +14,13 @@ var countdown = new Date("June 1,2024 0:00:00").getTime();
 var x = setInterval(function(){
     var now = new Date().getTime();
     var distance = countdown - now;
-    document.getElementById('countdown').innerHTML = days +'Days' + hours + 'Hours' + minutes + 'Minutes' + seconds + 'Seconds';
 
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    document.getElementById('countdown').innerHTML = days +'Days' + hours + 'Hours' + minutes + 'Minutes' + seconds + 'Seconds';
 
     if (distance < 0) {
       clearInterval(x);
