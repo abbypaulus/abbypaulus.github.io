905b8254892d0fbeb398168d0d318bb4e5613c32
Abigail Paulus <91631177+abbypaulus@users.noreply.github.com>
2023-09-21 07:41:15 -0400

    timer fixed and aligned

--- script.js
+++ script.js
@@ -10,21 +10,32 @@ addEventListener('resize', () => {
 c.rect(200,200,200,200);
 c.fill(255,0,0);
 
-var countdown = new Date("June 1,2024 0:00:00").getTime();
-var x = setInterval(function(){
-    var now = new Date().getTime();
-    var distance = countdown - now;
 
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
     
-    document.getElementById('countdown').innerHTML = days +'Days' + hours + 'Hours' + minutes + 'Minutes' + seconds + 'Seconds';
-
-    if (distance < 0) {
-      clearInterval(x);
-      document.getElementById('countdown').innerHTML = 'Expired';
-    }
-
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
 }, 1000);
