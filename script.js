35cfc209d3b35455fa1d13bffac738e8e86cdde1
Abigail Paulus <abbypaulus@icloud.com>
2024-10-15 16:13:44 -0400

    fixed api

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ for(let i = 0; i < 15; i++){
 
 document.addEventListener("adobe_dc_view_sdk.ready", function()
 {
-var adobeDCView = new AdobeDC.View({clientId: "aef28bece6ad47a081b57412a4ba6003", divId: "adobe-dc-view"});
+var adobeDCView = new AdobeDC.View({clientId: "cfe8c956b365414aaf54fd6567d63f8f", divId: "adobe-dc-view"});
 adobeDCView.previewFile(
 {
-content:   {location: {url: "https://acrobatservices.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf"}},
+content:   {location: {url: "https://abbypaulus.github.io/Resources/Paulus_Resume.pdf"}},
 metaData: {fileName: "Paulus_Resume.pdf"}
 });
 });
