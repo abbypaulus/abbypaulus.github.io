41ad9f866b0210096281e6ec5804b318b716015a
Abigail Paulus <abbypaulus@icloud.com>
2024-10-15 16:08:32 -0400

    update tagline, resume, add links page

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ for(let i = 0; i < 15; i++){
     
     //document.getElementById('typeEffect').innerHTML = abigail_paulus;
 }
+
+document.addEventListener("adobe_dc_view_sdk.ready", function()
+{
+var adobeDCView = new AdobeDC.View({clientId: "aef28bece6ad47a081b57412a4ba6003", divId: "adobe-dc-view"});
+adobeDCView.previewFile(
+{
+content:   {location: {url: "https://acrobatservices.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf"}},
+metaData: {fileName: "Paulus_Resume.pdf"}
+});
+});
