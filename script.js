8effc65ef7c31d9c1a4247a71c55e7a77200c6f9
Abigail Paulus <91631177+abbypaulus@users.noreply.github.com>
2025-06-06 09:55:58 -0400

    fix footer function

--- script.js
+++ script.js
@@ -19,11 +19,8 @@ metaData: {fileName: "Paulus_Resume.pdf"}
 window.onload = function(){
     var year = new Date().getFullYear();
     document.getElementById("currentYear").innerHTML = year;
+    ('#footer').load('footer.html') 
 }
 
 //tagline
 document.getElementById("tagline").innerHTML = "I am currently an Engineering Intern at Dometic, Programing Teacher at Code/Art, Student at Florida Atlantic University and Atlantic Technical College. Ambitious, detail-oriented mechanical engineering student focused on drafting, aspiring to be a design engineer."
-
-$(function(){
-    $('#footer').load('footer.html') 
-})
