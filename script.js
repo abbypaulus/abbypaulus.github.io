6e1461c3a2a6abb1b4e68e6bf3e5d98a5f498a2c
Abigail Paulus <91631177+abbypaulus@users.noreply.github.com>
2025-06-06 10:33:16 -0400

    fix tagline

--- script.js
+++ script.js
@@ -5,22 +5,13 @@ for(let i = 0; i < 15; i++){
     //document.getElementById('typeEffect').innerHTML = abigail_paulus;
 }
 
-document.addEventListener("adobe_dc_view_sdk.ready", function()
-{
-var adobeDCView = new AdobeDC.View({clientId: "cfe8c956b365414aaf54fd6567d63f8f", divId: "adobe-dc-view"});
-adobeDCView.previewFile(
-{
-content:   {location: {url: "https://abbypaulus.github.io/Resources/Paulus_Resume.pdf"}},
-metaData: {fileName: "Paulus_Resume.pdf"}
-});
-});
 
 //Get current date
 window.onload = function(){
     var year = new Date().getFullYear();
     document.getElementById("currentYear").innerHTML = year;
     ('#footer').load('footer.html') 
-}
-
 //tagline
 document.getElementById("tagline").innerHTML = "I am currently an Engineering Intern at Dometic, Programing Teacher at Code/Art, Student at Florida Atlantic University and Atlantic Technical College. Ambitious, detail-oriented mechanical engineering student focused on drafting, aspiring to be a design engineer."
+}
+
