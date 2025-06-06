0fd4a5fc75d4fdeabbb5ee0d1d42950fd1cba76f
Abigail Paulus <91631177+abbypaulus@users.noreply.github.com>
2025-06-06 11:18:44 -0400

    fix tagline

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ for(let i = 0; i < 15; i++){
     typeEffect.push('a');
     
     //document.getElementById('typeEffect').innerHTML = abigail_paulus;
-}
+};
 
 
 window.onload = function(){
@@ -11,7 +11,7 @@ window.onload = function(){
     var year = new Date().getFullYear();
     document.getElementById("currentYear").innerHTML = year;
     //tagline
-    var textTagline = "I am currently an Engineering Intern at Dometic, Programing Teacher at Code/Art, Student at Florida Atlantic University and Atlantic Technical College. Ambitious, detail-oriented mechanical engineering student focused on drafting, aspiring to be a design engineer.";
+    var taglineText = "I am currently an Engineering Intern at Dometic, Programing Teacher at Code/Art, Student at Florida Atlantic University and Atlantic Technical College. Ambitious, detail-oriented mechanical engineering student focused on drafting, aspiring to be a design engineer.";
     document.getElementById("tagline").innerHTML = taglineText;
     ('#footer').load('footer.html');
-}
+};
