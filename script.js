ef09c5b3aa253997c9f4d34b9624c7325eee7797
Abigail Paulus <91631177+abbypaulus@users.noreply.github.com>
2025-06-06 11:42:45 -0400

    test tagline

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ for(let i = 0; i < 15; i++){
     //document.getElementById('typeEffect').innerHTML = abigail_paulus;
 };
 
-
 window.onload = function(){
     //Get current date
     var year = new Date().getFullYear();
     document.getElementById("currentYear").innerHTML = year;
     //tagline
-    var taglineText = "I am currently an Engineering Intern at Dometic, Programing Teacher at Code/Art, Student at Florida Atlantic University and Atlantic Technical College. Ambitious, detail-oriented mechanical engineering student focused on drafting, aspiring to be a design engineer.";
-    document.getElementById("tagline").innerHTML = taglineText;
-    ('#footer').load('footer.html');
+    // var taglineText = "I am currently an Engineering Intern at Dometic, Programing Teacher at Code/Art, Student at Florida Atlantic University and Atlantic Technical College. Ambitious, detail-oriented mechanical engineering student focused on drafting, aspiring to be a design engineer.";
+    // document.getElementById("tagline").innerHTML = taglineText;
+    // ('#footer').load('footer.html');
+    const taglineText = document.getElementById("tagline").innerHTML = "I am currently an Engineering Intern at Dometic, Programing Teacher at Code/Art, Student at Florida Atlantic University and Atlantic Technical College. Ambitious, detail-oriented mechanical engineering student focused on drafting, aspiring to be a design engineer.";
 };
