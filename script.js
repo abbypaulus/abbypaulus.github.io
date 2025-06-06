960329033d57e2fe680fef0688a8ba6190690dde
Abigail Paulus <91631177+abbypaulus@users.noreply.github.com>
2025-06-06 08:10:59 -0400

    Update tagline, fix resume

--- script.js
+++ script.js
@@ -20,3 +20,7 @@ window.onload = function(){
     var year = new Date().getFullYear();
     document.getElementById("currentYear").innerHTML = year;
 }
+
+//tagline
+
+document.getElementById.("tagline").innerHTML = "I am currently an Engineering Intern at Dometic, Programing Teacher at Code/Art, Student at Florida Atlantic University and Atlantic Technical College. Ambitious, detail-oriented mechanical engineering student focused on drafting, aspiring to be a design engineer."
